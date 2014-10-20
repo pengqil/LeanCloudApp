bf0ccc7554d57aa33d50d67c1e74871acf83085a
Pengqi Liu <lpq1990@gmail.com>
2014-10-19 22:07:40 -0700

    Add AV Initialization.

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,7 @@
 require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
+AV.initialize("21dtlt787noxsxkgjxryjlsx52m7cv0e1752y73qa22iiboe", "qems0dyruiky9qrfpjmc6cufg68kp6ech4dvsya6e41aboz4");
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
